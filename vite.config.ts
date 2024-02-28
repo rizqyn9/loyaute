fff32d1c47d7b38764553d3e35c5880bb3b472c4
Rizqy Nugroho <rizqynugroho9@gmail.com>
2024-02-28 09:13:37 +0700

    feat: loyaute

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import Unfonts from "unplugin-fonts/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    open: true,
+    open: false,
     port: 3005,
   },
   test: {
